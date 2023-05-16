8eb7293d4800f4ef886d28f5f28eca3fd0572c1f
santiago <satiframe@gmail.com>
2023-05-16 13:03:04 -0300

    part1 1.4

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,46 +1,27 @@
 import Header from "./Header";
 import Content from "./Content";
-/* import Total from "./Total"; */
 
 export default function App() {
   const course = 'Half Stack application development'
-  const part1 = {
-    name: "Fundamentals of React",
-    exercises: 10,
-  }
-  /* const part1 = 'Fundamentals of React'
-  const exercises1 = 10 */
-
-  const part2 = {
-    name: "Using props to pass data",
-    exercises: 7,
-  }
-  /* const part2 = 'Using props to pass data'
-  const exercises2 = 7 */
-  
-
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
 
   return (
     <div>
       <Header course={course} />
-      <Content part={part1}/>
-      <Content part={part2}/>
-
-      {/* <Content 
-        part1={part1}
-        exercises1={exercises1}
-        part2={part2}
-        exercises2={exercises2}
-        part3={part3}
-        exercises3={exercises3}
-      />
-      <Total 
-        exercises1={exercises1}
-        exercises2={exercises2}
-        exercises3={exercises3}
-      /> */}
+      <Content parts={parts}/>
     </div>
   )
 }
--- part1/courseinfo/src/Content.js
+++ part1/courseinfo/src/Content.js
@@ -1,33 +1,9 @@
-/* function Part(props) {
-  return (
-    <p>{props.part} {props.exercises}</p>
-  )
-} */
-
 export default function Content(props) {
   return (
     <div>
-      <p> {props.part.name} {props.part.exercises} </p>
+      <p> {props.parts[0].name} {props.parts[0].exercises} </p>
+      <p> {props.parts[1].name} {props.parts[1].exercises} </p>
+      <p> {props.parts[2].name} {props.parts[2].exercises} </p>
     </div>
   )
 }
-
-/* 
-While using props can be cleaner, destructuring is often easier to understand
-
-function Part({ part, exercises }) {
-  return (
-    <p>{part} {exercises}</p>
-  )
-}
-
-export default function Content({ part1, exercises1, part2, exercises2, part3, exercises3 }) {
-  return (
-    <div>
-      <Part part={part1} exercises={exercises1}/>
-      <Part part={part2} exercises={exercises2}/>
-      <Part part={part3} exercises={exercises3}/>
-    </div>
-  )
-} 
-*/
